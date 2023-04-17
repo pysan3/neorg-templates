a9507e12dfbf04a9542f64dece2576cd46873c47
pysan3 <pysan3@gmail.com>
2023-04-18 03:16:44 +0900

    successfully load template files with Neorg

--- lua/neorg/modules/external/templates/module.lua
+++ lua/neorg/modules/external/templates/module.lua
@@ -3,19 +3,38 @@ require("neorg.modules")
 require("neorg.external.helpers")
 
 local ext_name = "templates"
+
+---Concat table of strings
+---@param s_tbl string | string[] # table of strings, if it is a simple string, treats as `{ s_tbl }`
+---@param separator string? # separator, defaults to `"."`
+---@param sep_at_front boolean? # whether to add separator at front
+---@param sep_at_end boolean? # whether to add separator at end
+---@return string
+local function join(s_tbl, separator, sep_at_front, sep_at_end)
+    ---@type string[]
+    local tbl = type(s_tbl) == "string" and { s_tbl } or s_tbl or {} ---@diagnostic disable-line
+    local sep = #tbl > 0 and (separator or ".") or ""
+    return (sep_at_front and sep or "") .. table.concat(tbl, sep) .. (sep_at_end and sep or "")
+end
+
 ---keynames based on `ext_name`
----@param post string? # keys coming after `ext_name`
----@param pre string? # keys coming after `ext_name`
+---@param post string | string[] | nil # keys coming after `ext_name`
+---@param pre string | string[] | nil # keys coming after `ext_name`
 ---@return string
 local function plug(post, pre)
-    local s = ext_name
-    if pre then
-        s = pre .. "." .. s
+    return join(pre or {}, ".", false, true) .. ext_name .. join(post or {}, ".", true, false)
+end
+---keynames based on `ext_name`
+---@param s string # string containing `ext_name`
+---@return string[], string[] # `pre` and `post`
+local function deplug(s)
+    local splits = vim.split(s, ".", { plain = true, trimempty = true })
+    for i, split in ipairs(splits) do
+        if split == ext_name then
+            return { unpack(splits, 1, i - 1) }, { unpack(splits, i + 1) }
+        end
     end
-    if post then
-        s = s .. "." .. post
-    end
-    return s
+    return {}, splits
 end
 
 local module = neorg.modules.create(plug(nil, "external")) ---@diagnostic disable-line
@@ -38,50 +57,24 @@ end
 
 module.config.public = {
     templates_dir = vim.fn.stdpath("config") .. "/templates/norg",
+    default_subcommand = "add",
     keywords = {},
 }
 
 module.private = {
-    templates_dir = "",
+    templates_dir_abs = "",
     template_files = {},
 }
 
-module.private.define_commands = function()
-    local cmds = {
-        definitions = {
-            templates = {
-                load = {},
-                fload = {},
-                add = {},
-            },
-        },
-        data = {
-            templates = {
-                min_args = 2,
-                max_args = 3,
-                subcommands = {
-                    load = { args = 1, name = plug("load") },
-                    fload = { args = 1, name = plug("fload") },
-                    add = { args = 1, name = plug("add") },
-                },
-            },
-        },
-    }
-    for fs_name, _ in pairs(module.private.template_files) do
-        local cmd_name = plug(fs_name)
-        cmds.definitions.templates[fs_name] = {}
-        cmds.data.templates.subcommands[fs_name] = { args = 0, name = cmd_name }
-        module.events.subscribed["core.neorgcmd"] = {
-            [cmd_name] = true,
-        }
-    end
-    module.required["core.neorgcmd"].add_commands_from_table(cmds)
-end
+---@type { [string]: fun(fs_name: string) } # function takes `fs_name` which is the name of template file
+-- `fs_name = journal` will load (<module.config.public.templates_dir>/<fs_name>.norg) and load with LuaSnip
+module.private.subcommands = {}
 
-module.private[plug("add")] = function(fs_name) ---@diagnostic disable-line
+---Add (append) template file content to the current cursor position
+module.private.subcommands.add = function(fs_name)
     for name, path in pairs(module.private.template_files) do
         if fs_name == name then
-            local abs_path = vim.fn.fnamemodify(path, ":p")
+            local abs_path = module.private.templates_dir_abs .. "/" .. path
             local st = uv.fs_stat(abs_path)
             local file_content = ""
             if not st then
@@ -95,51 +88,92 @@ module.private[plug("add")] = function(fs_name) ---@diagnostic disable-line
                     end
                 end
             end
-            return snippet_handler.load_template_at_curpos(file_content)
+            return snippet_handler.load_template_at_curpos(file_content, fs_name)
         end
     end
+    -- no template file with `fs_name` found
+    log.warn("Aborting. No template file found: " .. fs_name)
 end
 
-module.private[plug("fload")] = function(fs_name) ---@diagnostic disable-line
-    vim.cmd("normal! VggGd") -- delete content inside file
-    return module.private[plug("add")](fs_name)
+---Force-load fs_name. Will overwrite content of current file and replace it with LuaSnip
+module.private.subcommands.fload = function(fs_name)
+    vim.cmd("normal! ggVGd") -- delete content inside file
+    return module.private.subcommands.add(fs_name)
 end
 
-module.private[plug("load")] = function(fs_name) ---@diagnostic disable-line
+---Looks at `module.private.commands` and files inside `module.config.public.templates_dir` to create the subcommands
+---This dynamically creates the completion options after `:Neorg templates <subcommand> <fs_name>`
+module.private.define_commands = function()
+    local subscribed_neorgcmd = {}
+    local cmds = { templates = { args = 3, subcommands = {} } }
+    for fs_name, _ in pairs(module.private.template_files) do
+        local cmd_name = plug(fs_name)
+        subscribed_neorgcmd[cmd_name] = true
+        cmds.templates.subcommands[fs_name] = { args = 0, name = cmd_name }
+        for command_name, _ in pairs(module.private.subcommands) do
+            if not cmds.templates.subcommands[command_name] then
+                cmds.templates.subcommands[command_name] = { args = 1, subcommands = {}, name = plug(command_name) }
+            end
+            cmds.templates.subcommands[command_name].subcommands[fs_name] =
+                { args = 0, name = plug({ "load", fs_name }) }
+            subscribed_neorgcmd[plug({ command_name, fs_name })] = true
+        end
+    end
+    module.required["core.neorgcmd"].add_commands_from_table(cmds)
+    module.events.subscribed = { ["core.neorgcmd"] = subscribed_neorgcmd }
+end
+
+---Load. Similar to `fload` but asks for confirmation before deleting buffer content
+module.private.subcommands.load = function(fs_name)
     local help = "(use `:Neorg templates add xxx` to append template file)"
     local msg = "Current buffer has contents. Delete? " .. help
     if utils.buffer_has_contents(0) and not utils.confirm(msg) then
         return
     end
-    return module.private[plug("fload")](fs_name)
+    return module.private.subcommands.fload(fs_name)
 end
 
 ---First function to be loaded
 module.load = function()
     -- Find templates
-    module.private.templates_dir = vim.fn.fnamemodify(module.config.public.templates_dir, ":p")
-    vim.notify([[Loading templates from: ]] .. module.private.templates_dir) -- debug
-    if not utils.fs_exists(module.private.templates_dir) then
-        log.warn([[templates_dir does not exist: ]] .. module.private.templates_dir)
+    module.private.templates_dir_abs = vim.fn.fnamemodify(module.config.public.templates_dir, ":p")
+    log.debug([[Loading templates from: ]] .. module.private.templates_dir_abs)
+    if not utils.fs_exists(module.private.templates_dir_abs) then
+        log.warn([[templates_dir does not exist: ]] .. module.private.templates_dir_abs)
     end
-    for _, path in ipairs(utils.list_template_files(module.private.templates_dir)) do
+    for _, path in ipairs(utils.list_template_files(module.private.templates_dir_abs)) do
         local fs_name = utils.fs_name(path)
         module.private.template_files[fs_name] = path
     end
-    vim.notify("Template files found: " .. vim.inspect(module.private.template_files)) -- debug
+    log.debug("Template files found: " .. vim.inspect(module.private.template_files))
     module.private.define_commands()
+
+    -- Check `module.config.public.default_subcommand` is valid
+    if not vim.tbl_contains(vim.tbl_keys(module.private.subcommands), module.config.public.default_subcommand) then
+        log.warn(
+            [[`config.default_subcommand` is not a valid value: ]]
+                .. module.config.public.default_subcommand
+                .. string.format(
+                    [[ Should be one of: %s. Defaulting to `add`]],
+                    vim.tbl_keys(module.private.subcommands)
+                )
+        )
+        module.config.public.default_subcommand = "add"
+    end
 
     -- Append keywords
     snippet_handler.add_keywords(module.config.public.keywords or {})
-    vim.notify(vim.inspect(vim.tbl_keys(snippet_handler.keywords)))
 end
 
 module.on_event = function(event)
     if vim.tbl_contains({ "core.keybinds", "core.neorgcmd" }, event.split_type[1]) then
-        if module.private[plug(event.split_type[2])] ~= nil then
-            return module.private[plug(event.split_type[2])](event.content[1])
+        local _, post = deplug(event.split_type[2])
+        local func, fs_name = unpack(post)
+        if module.private.subcommands[func] ~= nil then
+            return module.private.subcommands[func](fs_name)
         end
-        return module.private[plug("add")](event.split_type[2])
+        -- Directry specify fs_name. (:Neorg templates fs_name) -> (:Neorg templates add fs_name)
+        return module.private.subcommands[module.config.public.default_subcommand](func)
     end
 end
 
--- lua/neorg/modules/external/templates/snippet_handler.lua
+++ lua/neorg/modules/external/templates/snippet_handler.lua
@@ -94,6 +94,66 @@ M.add_keywords = function(kwds)
     M.keywords = vim.tbl_extend("force", M.default_keywords, M.keywords, kwds)
 end
 
-M.load_template_at_curpos = function(content) end
+M.search_keywords = function(content)
+    local kwds_ids = { TITLE = 1 }
+    for key, _ in pairs(M.keywords) do
+        if not kwds_ids[key] then
+            local splits = vim.split(content, key, { plain = true, trimempty = false })
+            kwds_ids[key] = #splits > 1 and string.len(splits[1]) or nil
+        end
+    end
+    return kwds_ids
+end
+
+M.build_keywords = function(kwds_ids)
+    local res = { CURSOR = i(0) }
+    for key, id in pairs(kwds_ids) do
+        -- res[key] = d(id, function(args, parent, old_stage, user_args)
+        --     return sn(1, M.keywords[key])
+        -- end)
+        res[key] = sn(id, { M.keywords[key] })
+    end
+    return res
+end
+
+M.create_snippet = function(content, snip_name)
+    local dscr = "Created by norg_snippet_handler: " .. snip_name
+    local kwds_ids = M.search_keywords(content)
+    local keywords = M.build_keywords(kwds_ids)
+    return s(e(snip_name, snip_name, dscr), fmt(content, keywords, { strict = false }))
+end
+
+M.add_snippet_to_luasnip = function(content, fs_name, add_opts)
+    local ft = "norg"
+    local file = "norg_snippet_handler." .. fs_name .. ".norg"
+    local type = "snippets"
+    local snip_name = "__snippets_" .. file
+    -- add temporary snippet
+    ls.add_snippets(
+        ft,
+        { M.create_snippet(content, snip_name) },
+        vim.tbl_extend("keep", {
+            type = type,
+            key = snip_name,
+            -- prevent refresh here, will be done outside loop.
+            refresh_notify = false,
+        }, add_opts or {})
+    )
+    -- get new snippet object
+    local snippet_list = ls.get_snippets(ft, type)
+    for _, snip in ipairs(snippet_list) do
+        if snip.name == snip_name then
+            return snip
+        end
+    end
+end
+
+M.load_template_at_curpos = function(content, fs_name)
+    local snip = M.add_snippet_to_luasnip(content, fs_name, {})
+    vim.cmd.startinsert()
+    vim.schedule(function()
+        ls.snip_expand(snip, {})
+    end)
+end
 
 return M
